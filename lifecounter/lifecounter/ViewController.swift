9af16c423c9553b8091d6d190c2707d5fa6a37b1
SirBlank <amberyuewu@gmail.com>
2025-04-16 19:20:09 -0700

    add app template

---
+++ lifecounter/lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Amber Wu on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
